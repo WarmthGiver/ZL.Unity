5ccf939273d9aeb4fe725ef8590ca25f9cf85ebf
ZION <warmth.giver@gmail.com>
2025-04-21 17:18:09 +0900

    gitignore Modify

---
+++ Project/ZL.Unity/Assets/WorkSpace/ZL/Unity/Server/Firebase/Scripts/FirebaseAuthManager.cs
@@ -0,0 +1,150 @@
+using Firebase;
+
+using Firebase.Auth;
+
+using Firebase.Extensions;
+
+using System;
+
+using System.Collections;
+
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace ZL.Unity.Server.Firebase.Auth
+{
+    [AddComponentMenu("ZL/Server/Firebase/Firebase Auth Manager (Singleton)")]
+
+    //[DefaultExecutionOrder(-1)]
+
+    [DisallowMultipleComponent]
+
+    public sealed class FirebaseAuthManager : MonoSingleton<FirebaseAuthManager>
+    {
+        /*[Space]
+
+        [SerializeField]
+
+        [UsingCustomProperty]
+
+        [Alias("ID Input Field")]
+
+        private TMP_InputField idInputField;
+
+        [SerializeField]
+
+        [UsingCustomProperty]
+
+        [Alias("PW Input Field")]
+
+        private TMP_InputField pwInputField;
+
+        [Space]
+
+        [SerializeField]
+
+        private Button loginButton;*/
+
+        public FirebaseAuth Auth { get; private set; } = null;
+
+        public FirebaseUser User { get; private set; } = null;
+
+        protected override void Awake()
+        {
+            if (ISingleton<FirebaseAuthManager>.TrySetInstance(this) == false)
+            {
+                return;
+            }
+
+            FirebaseApp.
+
+            CheckAndFixDependenciesAsync().
+
+            ContinueWithOnMainThread((task) =>
+            {
+                if (task.Result == DependencyStatus.Available)
+                {
+                    Auth = FirebaseAuth.DefaultInstance;
+
+                    Debug.Log("Firebase Auth load successful.");
+                }
+
+                else
+                {
+                    Debug.LogError($"Firebase Auth load failed: {task.Result}");
+                }
+            });
+        }
+
+        public void SignIn(string id, string pw)
+        {
+            if (Auth == null)
+            {
+                return;
+            }
+
+            var task = Auth.SignInWithEmailAndPasswordAsync(id, pw);
+
+            var routine = task.WaitForCompleted((task) => User = task.Result.User);
+
+            StartCoroutine(routine);
+        }
+
+        public void SignUp(string id, string pw)
+        {
+            if (Auth == null)
+            {
+                return;
+            }
+
+            var task = Auth.CreateUserWithEmailAndPasswordAsync(id, pw);
+
+            var routine = task.WaitForCompleted((task) => User = task.Result.User);
+
+            StartCoroutine(routine);
+        }
+
+        public void UpdateUserProfile(UserProfile userProfile, Func<Task, IEnumerator> routine = null)
+        {
+            if (User == null)
+            {
+                return;
+            }
+
+            var task = User.UpdateUserProfileAsync(userProfile);
+
+            if (routine != null)
+            {
+                StartCoroutine(routine(task));
+            }
+        }
+    }
+
+    public static partial class TaskExtensions
+    {
+        public static IEnumerator WaitForCompleted<TTask>(this TTask instance, Action<TTask> callback = null)
+
+            where TTask : Task
+        {
+            while (instance.IsCompleted == false)
+            {
+                yield return null;
+            }
+
+            callback?.Invoke(instance);
+        }
+
+        public static bool IsExceptionThrown(this Task instance, out FirebaseException exception)
+        {
+            return IsExceptionThrown(instance, out exception);
+        }
+
+        public static bool IsExceptionThrown(this Task instance, out Exception exception)
+        {
+            exception = instance.Exception;
+
+            return exception != null;
+        }
+    }
+}
---
+++ Project/ZL.Unity/Assets/WorkSpace/ZL/Unity/Server/Firebase/Scripts/FirebaseDBManager.cs
@@ -0,0 +1,96 @@
+using Firebase;
+
+using Firebase.Auth;
+
+using Firebase.Database;
+
+using Firebase.Extensions;
+
+using System;
+
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace ZL.Unity.Server.Firebase.Auth
+{
+    [AddComponentMenu("ZL/Server/Firebase/Firebase DB Manager (Singleton)")]
+
+    [DefaultExecutionOrder(-1)]
+
+    [DisallowMultipleComponent]
+
+    public sealed class FirebaseDBManager : MonoSingleton<FirebaseDBManager>
+    {
+        public DatabaseReference Database { get; private set; } = null;
+
+        private FirebaseUser User;
+
+        protected override void Awake()
+        {
+            if (ISingleton<FirebaseDBManager>.TrySetInstance(this) == false)
+            {
+                return;
+            }
+
+            FirebaseApp.
+                
+            CheckAndFixDependenciesAsync().
+
+            ContinueWithOnMainThread((task) =>
+            {
+                if (task.Result == DependencyStatus.Available)
+                {
+                    Database = FirebaseDatabase.DefaultInstance.RootReference;
+
+                    Debug.Log("Firebase DB load Successful.");
+                }
+
+                else
+                {
+                    Debug.LogError($"Firebase DB load failed: {task.Result}");
+                }
+            });
+        }
+
+        private void Start()
+        {
+            User = ISingleton<FirebaseAuthManager>.Instance.User;
+        }
+
+        public void SetValue(object value, params string[] paths)
+        {
+            var database = Database.Child(paths);
+
+            var task = database.SetValueAsync(value);
+
+            var routine = task.WaitForCompleted();
+
+            StartCoroutine(routine);
+        }
+
+        public void GetValue(Action<Task<DataSnapshot>> callback, params string[] paths)
+        {
+            var database = Database.Child(paths);
+
+            var task = database.GetValueAsync();
+
+            var routine = task.WaitForCompleted(callback);
+
+            StartCoroutine(routine);
+        }
+    }
+
+    public static class DatabaseReferenceExtensions
+    {
+        public static DatabaseReference Child(this DatabaseReference instance, string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                instance = instance.Child(path);
+            }
+
+            return instance;
+        }
+    }
+}
